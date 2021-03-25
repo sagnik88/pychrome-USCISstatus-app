23299df0a6b8247526651720a127ad890da7177f
Sagnik Saha <sagnik@Sagniks-MacBook-Air.local>
2021-03-25 09:05:38 -0400

    Code cleanup - Removed sent mail and update table to functions

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -13,179 +13,114 @@ def lambda_handler(*args, **kwargs):
     driver = WebDriverWrapper()
     receiptnumber = os.getenv("RECEIPTNUMBER")
     now = datetime.datetime.now()
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    date_time = now.strftime("%Y%m%d")
     driver.get_url('https://egov.uscis.gov/casestatus/landing.do')
-    driver.set_input_value('/html/body/div[2]/form/div/div[1]/div/div[1]/fieldset/div[1]/div[4]/input',receiptnumber)
+    driver.set_input_value('/html/body/div[2]/form/div/div[1]/div/div[1]/fieldset/div[1]/div[4]/input', receiptnumber)
     driver.click('/html/body/div[2]/form/div/div[1]/div/div[1]/fieldset/div[2]/div[2]/input')
     example_text = driver.get_inner_html('(//div//h1)[1]')
-    print(example_text)
     client = boto3.resource('dynamodb')
     table = client.Table("receipt_status")
-    print(table.table_status)
-    # table.put_item(Item={'receipt_number': receiptnumber, 'update_date': date_time, 'receipt_status': example_text})
     driver.close()
     response = table.query(
-        KeyConditionExpression=Key('receipt_number').eq(receiptnumber)
+        KeyConditionExpression=Key('receipt_number').eq(receiptnumber),
+        ScanIndexForward=False
     )
     item = response['Items']
     receipt_status_old = item[0]["receipt_status"]
-    print(receipt_status_old)
+    last_updated = item[0]["update_date"]
     if example_text == receipt_status_old:
         status = "No change in status"
-        print("No change in status")
-        SENDER = "Sender Name <sagnik88@gmail.com>"
-
-        # Replace recipient@example.com with a "To" address. If your account
-        # is still in the sandbox, this address must be verified.
-        RECIPIENT = "sagnik88@gmail.com"
-
-        # Specify a configuration set. If you do not want to use a configuration
-        # set, comment the following variable, and the
-        # ConfigurationSetName=CONFIGURATION_SET argument below.
-        # CONFIGURATION_SET = "ConfigSet"
-
-        # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-        AWS_REGION = "us-east-1"
-
-        # The subject line for the email.
-        SUBJECT = "No Update to USCIS status for receipt number:" + receiptnumber
-
-        # The email body for recipients with non-HTML email clients.
-        BODY_TEXT = ("No Update to USCIS status for receipt number:" + receiptnumber + "\r\n" +
-                     "The current receipt status is " + example_text
-                     )
-
-        # The HTML body of the email.
-        BODY_HTML = """<html>
-                <head></head>
-                <body>
-                  <h1>No Update to USCIS status for receipt number: """ + receiptnumber + """</h1>
-                  <p>The current receipt status is: """ + example_text + """</p>
-                </body>
-                </html>
-                            """
-
-        # The character encoding for the email.
-        CHARSET = "UTF-8"
-
-        # Create a new SES resource and specify a region.
-        client = boto3.client('ses', region_name=AWS_REGION)
-
-        # Try to send the email.
-        try:
-            # Provide the contents of the email.
-            response = client.send_email(
-                Destination={
-                    'ToAddresses': [
-                        RECIPIENT,
-                    ],
-                },
-                Message={
-                    'Body': {
-                        'Html': {
-                            'Charset': CHARSET,
-                            'Data': BODY_HTML,
-                        },
-                        'Text': {
-                            'Charset': CHARSET,
-                            'Data': BODY_TEXT,
-                        },
-                    },
-                    'Subject': {
-                        'Charset': CHARSET,
-                        'Data': SUBJECT,
-                    },
-                },
-                Source=SENDER,
-                # If you are not using a configuration set, comment or delete the
-                # following line
-                # ConfigurationSetName=CONFIGURATION_SET,
-            )
-        # Display an error if something goes wrong.
-        except ClientError as e:
-            print(e.response['Error']['Message'])
-        else:
-            print("Email sent! Message ID:"),
-            print(response['MessageId'])
+        send_email('sagnik88@gmail.com', 'sagnik88@gmail.com', receiptnumber, example_text)
     else:
-        status="Change in status"
-        print("Change in status")
+        status = "Change in status"
+        update_item(receiptnumber, date_time, example_text)
+        send_email('sagnik88@gmail.com', 'sagnik88@gmail.com', receiptnumber, example_text)
         table.put_item(Item={'receipt_number': receiptnumber, 'update_date': date_time, 'receipt_status': example_text})
-        # Replace sender@example.com with your "From" address.
-        # This address must be verified with Amazon SES.
-        SENDER = "Sender Name <sagnik88@gmail.com>"
-
-        # Replace recipient@example.com with a "To" address. If your account
-        # is still in the sandbox, this address must be verified.
-        RECIPIENT = "sagnik88@gmail.com"
-
-        # Specify a configuration set. If you do not want to use a configuration
-        # set, comment the following variable, and the
-        # ConfigurationSetName=CONFIGURATION_SET argument below.
-        # CONFIGURATION_SET = "ConfigSet"
-
-        # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-        AWS_REGION = "us-east-1"
-
-        # The subject line for the email.
-        SUBJECT = "Update to USCIS status for receipt number:" + receiptnumber
-
-        # The email body for recipients with non-HTML email clients.
-        BODY_TEXT = ("Update to USCIS status for receipt number:" + receiptnumber + "\r\n" +
-                     "The receipt status has been updated to " + example_text
-                     )
-
-        # The HTML body of the email.
-        BODY_HTML = """<html>
-        <head></head>
-        <body>
-          <h1>Update to USCIS status for receipt number: """ + receiptnumber + """</h1>
-          <p>The receipt status has been updated to: """ + example_text + """</p>
-        </body>
-        </html>
-                    """
-
-        # The character encoding for the email.
-        CHARSET = "UTF-8"
-
-        # Create a new SES resource and specify a region.
-        client = boto3.client('ses', region_name=AWS_REGION)
-
-        # Try to send the email.
-        try:
-            # Provide the contents of the email.
-            response = client.send_email(
-                Destination={
-                    'ToAddresses': [
-                        RECIPIENT,
-                    ],
-                },
-                Message={
-                    'Body': {
-                        'Html': {
-                            'Charset': CHARSET,
-                            'Data': BODY_HTML,
-                        },
-                        'Text': {
-                            'Charset': CHARSET,
-                            'Data': BODY_TEXT,
-                        },
-                    },
-                    'Subject': {
-                        'Charset': CHARSET,
-                        'Data': SUBJECT,
-                    },
-                },
-                Source=SENDER,
-                # If you are not using a configuration set, comment or delete the
-                # following line
-                # ConfigurationSetName=CONFIGURATION_SET,
-            )
-        # Display an error if something goes wrong.
-        except ClientError as e:
-            print(e.response['Error']['Message'])
-        else:
-            print("Email sent! Message ID:"),
-            print(response['MessageId'])
-
     return example_text
+
+
+def update_item(receiptnumber, update_date, receiptstatus_val):
+    try:
+        dynamodb = boto3.resource('dynamodb', 'us-east-1')
+        table = dynamodb.Table('receipt_status')
+        set_update_date = str(update_date)
+        response = table.update_item(
+            Key={
+                "receipt_number": receiptnumber,
+                "update_date": str(update_date)
+            },
+            UpdateExpression="set receipt_status=:new_receipt_status",
+            ExpressionAttributeValues={
+                ":new_receipt_status": receiptstatus_val
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    except Exception as msg:
+        print(f"Oops, could not update: {msg}")
+
+
+def send_email(sender_email, recipient_email, receipt_no, receipt_stat):
+    sender = "Sender Name <"+sender_email+">"
+    recipient = recipient_email
+    aws_region = "us-east-1"
+
+    # The subject line for the email.
+    subject = "Update to USCIS status for receipt number:" + receipt_no
+
+    # The email body for recipients with non-HTML email clients.
+    body_text = ("Update to USCIS status for receipt number:" + receipt_no + "\r\n" +
+                 "The receipt status has been updated to " + receipt_stat
+                 )
+
+    # The HTML body of the email.
+    body_html = """<html>
+     <head></head>
+     <body>
+       <h1>Update to USCIS status for receipt number: """ + receipt_no + """</h1>
+       <p>The receipt status has been updated to: """ + receipt_stat + """</p>
+     </body>
+     </html>
+                 """
+
+    # The character encoding for the email.
+    charset = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses', region_name=aws_region)
+
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    recipient,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': charset,
+                        'Data': body_html,
+                    },
+                    'Text': {
+                        'Charset': charset,
+                        'Data': body_text,
+                    },
+                },
+                'Subject': {
+                    'Charset': charset,
+                    'Data': subject,
+                },
+            },
+            Source=sender,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+            # ConfigurationSetName=CONFIGURATION_SET,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
